626304b5de25fea43e16f1654d00df907a4b4e69
angela peralta <233649781+compugames3@users.noreply.github.com>
2026-03-25 10:30:23 +0000

    Generated by Spark: arreglalo

---
+++ preload.cjs
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  readDB: () => ipcRenderer.invoke('read-db'),
+  writeDB: (data) => ipcRenderer.invoke('write-db', data),
+  getDBPath: () => ipcRenderer.invoke('get-db-path')
+});
