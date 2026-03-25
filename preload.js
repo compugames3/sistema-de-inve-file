3fd4bedc29302a700c8234c6ddc81fc5ddd6287c
angela peralta <233649781+compugames3@users.noreply.github.com>
2026-03-25 09:13:30 +0000

    Edited Spark

--- preload.js
+++ preload.js
@@ -2,8 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   readDB: () => ipcRenderer.invoke('read-db'),
-  writeDB: (data) => ipcRenderer.invoke('write-db', data),
-  getDBPath: () => ipcRenderer.invoke('get-db-path'),
-  platform: process.platform,
-  isElectron: true
+});
+
+
+
 });
